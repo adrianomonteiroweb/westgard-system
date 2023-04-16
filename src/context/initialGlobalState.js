6b522c9e65f87cc5d3d529df3d9801100d8018f6
Adriano Monteiro <adrianomonteiroweb@gmail.com>
2023-04-15 23:39:37 -0300

    new: clear stage1 method

---
+++ src/context/initialGlobalState.js
@@ -0,0 +1,22 @@
+export const initialStage1 = {
+    analiticSystem: '',
+        test: '',
+        unit: '',
+        method: '',
+        period: ''
+};
+
+export const initialStage2 = {
+    nivel1: {
+        nivel1: '',
+        batchNumber: 0,
+        media: 100,
+        DP: 2
+    },
+    nivel2: {
+        nivel2: '',
+        batchNumber: 0,
+        media: 300,
+        DP: 4
+    }
+};
