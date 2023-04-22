d20f7f32a7f7cf5cff8a801ba9d67fb752975396
Adriano Monteiro <adrianomonteiroweb@gmail.com>
2023-04-22 15:46:45 -0300

    fix: analitic page

--- src/pages/AnaliticSystemPage/AnaliticSystemPage.js
+++ src/pages/AnaliticSystemPage/AnaliticSystemPage.js
@@ -1,34 +1,32 @@
 import { useContext } from 'react';
-import Form from 'react-bootstrap/Form';
 import { Link } from 'react-router-dom';
 
 import IsContext from '../../context/IsContext';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './analiticSystemPage.css'
 import { initialStage1 } from '../../context/initialGlobalState';
+import InputComponent from '../../components/forms/inputs/InputComponent';
 
 function AnaliticSystemPage() {
     const { stage1, setStage1 } = useContext(IsContext);
-    
+
+    console.log(stage1);
+
+    const setStage1Func = (event, prop) =>
+      setStage1({...stage1, [prop]: event.target.value })
+
     return (
       <div className='analiticSystem-div'>
-        <h5>Estágio 1</h5>
-        {/* analiticSystem input */}
-        <Form.Label htmlFor='analiticSystem'>Sistema Analítico</Form.Label>
-        <Form.Control type='text' id='analiticSystem' value={ stage1.analiticSystem } onChange={(event) => setStage1({...stage1, analiticSystem: event.target.value })} />
+        {/* analitic input */}
+        <InputComponent input={['text', 'analiticSystem', stage1.analiticSystem, 'Sistema Analítico', setStage1Func, 'analiticSystem']} />
         {/* test input */}
-        <Form.Label htmlFor='test'>Teste</Form.Label>
-        <Form.Control type='text' id='test' value={ stage1.test } onChange={(event) => setStage1({...stage1, test: event.target.value })} />
+        <InputComponent input={['text', 'test', stage1.test, 'Teste', setStage1Func, 'test']} />
         {/* unit input */}
-        <Form.Label htmlFor='unit'>Unidade</Form.Label>
-        <Form.Control type='text' id='unit' value={ stage1.unit } onChange={(event) => setStage1({...stage1, unit: event.target.value })} />
+        <InputComponent input={['text', 'unit', stage1.unit, 'Unidade', setStage1Func, 'unit']} />
         {/* method input */}
-        <Form.Label htmlFor='method'>Método</Form.Label>
-        <Form.Control type='text' id='method' value={ stage1.method } onChange={(event) => setStage1({...stage1, method: event.target.value })} />
+        <InputComponent input={['text', 'method', stage1.method, 'Método', setStage1Func, 'method']} />
         {/* period input */}
-        <Form.Label htmlFor='period'>Período</Form.Label>
-        <Form.Control type='text' id='period' value={ stage1.period } onChange={(event) => setStage1({...stage1, period: event.target.value })} />
+        <InputComponent input={['date', 'period', stage1.period, 'Período', setStage1Func, 'period']} />
         {/* link */}
         <Link to='/batch-record' id='next-button'>Próximo</Link>
         <Link id='clear-button' onClick={() => setStage1(initialStage1)}>Limpar</Link>
