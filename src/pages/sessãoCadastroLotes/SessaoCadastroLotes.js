cdf0e27de63abc413b20ae1cf8b043a62396f730
Adriano Monteiro <adrianomonteiroweb@gmail.com>
2023-10-01 15:55:02 -0300

    new: settings

--- src/pages/sessãoCadastroLotes/SessaoCadastroLotes.js
+++ src/pages/sessãoCadastroLotes/SessaoCadastroLotes.js
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function SessaoCadastroLotes() {
-  const [lotes, setLotes] = useState([]);
-  const [currentIndex, setCurrentIndex] = useState(0);
   const [novoLote, setNovoLote] = useState({
     analise: "",
     numeroLote: "",
@@ -11,59 +9,71 @@ function SessaoCadastroLotes() {
     desvioPadrao: "",
   });
 
-  useEffect(() => {
-    const lotesSalvos = JSON.parse(localStorage.getItem("lotesDados"));
-    if (lotesSalvos) {
-      setLotes(lotesSalvos);
-    }
-  }, []);
+  const [lotes, setLotes] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  const persistirDados = () => {
-    localStorage.setItem("lotesDados", JSON.stringify(lotes));
+  useEffect(() => {
+    const lotesSalvos = JSON.parse(localStorage.getItem("lotesDados")) || [];
+    setLotes(lotesSalvos);
+    setNovoLote(lotesSalvos[currentIndex] || {});
+  }, [currentIndex]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNovoLote({
+      ...novoLote,
+      [name]: value,
+    });
   };
 
-  useEffect(() => {
-    persistirDados();
-  }, [lotes]);
-
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-
+  const handleNextLote = () => {
     if (
-      novoLote.analise.trim() &&
-      novoLote.numeroLote.trim() &&
-      novoLote.media.trim() &&
-      novoLote.desvioPadrao.trim()
+      !novoLote.analise ||
+      !novoLote.numeroLote ||
+      !novoLote.media ||
+      !novoLote.desvioPadrao
     ) {
-      setLotes([...lotes, novoLote]);
-      setNovoLote({
-        analise: "",
-        numeroLote: "",
-        media: "",
-        desvioPadrao: "",
-      });
+      alert("Preencha todos os campos obrigatórios.");
+      return;
     }
+
+    // Salvar lote existente
+    const novosLotes = [...lotes];
+    novosLotes[currentIndex] = novoLote;
+    setLotes(novosLotes);
+    localStorage.setItem("lotesDados", JSON.stringify(novosLotes));
+
+    setNovoLote({
+      analise: "",
+      numeroLote: "",
+      media: "",
+      desvioPadrao: "",
+    });
+    setCurrentIndex(currentIndex + 1);
   };
 
   const handlePreviousLote = () => {
     if (currentIndex > 0) {
       setCurrentIndex(currentIndex - 1);
-      setNovoLote(lotes[currentIndex - 1]);
     }
   };
 
-  const handleNextLote = () => {
-    if (currentIndex < lotes.length - 1) {
-      setCurrentIndex(currentIndex + 1);
-      setNovoLote(lotes[currentIndex + 1]);
-    } else {
-      setCurrentIndex(currentIndex + 1);
-      setNovoLote({
-        analise: "",
-        numeroLote: "",
-        media: "",
-        desvioPadrao: "",
-      });
+  const handleDeleteLote = () => {
+    const shouldDelete = window.confirm(
+      "Tem certeza de que deseja excluir este lote?"
+    );
+
+    if (shouldDelete) {
+      const novosLotes = [...lotes];
+      novosLotes.splice(currentIndex, 1);
+      setLotes(novosLotes);
+      localStorage.setItem("lotesDados", JSON.stringify(novosLotes));
+
+      if (currentIndex > 0) {
+        setCurrentIndex(novosLotes.length - 1);
+      } else if (novosLotes.length > 0) {
+        setCurrentIndex(0);
+      }
     }
   };
 
@@ -75,7 +85,7 @@ function SessaoCadastroLotes() {
             <h2 className="mt-1">Controle de Qualidade</h2>
           </div>
           <h2 className="mb-1">Sessão: Cadastro de Lotes</h2>
-          <form onSubmit={handleFormSubmit}>
+          <form>
             <div className="mb-1">
               <label htmlFor="analise" className="form-label">
                 Análise:
@@ -84,10 +94,9 @@ function SessaoCadastroLotes() {
                 type="text"
                 className="form-control"
                 id="analise"
+                name="analise"
                 value={novoLote.analise}
-                onChange={(e) =>
-                  setNovoLote({ ...novoLote, analise: e.target.value })
-                }
+                onChange={handleInputChange}
                 required
                 placeholder="Insira a análise"
               />
@@ -100,10 +109,9 @@ function SessaoCadastroLotes() {
                 type="text"
                 className="form-control"
                 id="numeroLote"
+                name="numeroLote"
                 value={novoLote.numeroLote}
-                onChange={(e) =>
-                  setNovoLote({ ...novoLote, numeroLote: e.target.value })
-                }
+                onChange={handleInputChange}
                 required
                 placeholder="Insira o número de lote"
               />
@@ -116,10 +124,9 @@ function SessaoCadastroLotes() {
                 type="text"
                 className="form-control"
                 id="media"
+                name="media"
                 value={novoLote.media}
-                onChange={(e) =>
-                  setNovoLote({ ...novoLote, media: e.target.value })
-                }
+                onChange={handleInputChange}
                 required
                 placeholder="Insira a média"
               />
@@ -132,37 +139,71 @@ function SessaoCadastroLotes() {
                 type="text"
                 className="form-control"
                 id="desvioPadrao"
+                name="desvioPadrao"
                 value={novoLote.desvioPadrao}
-                onChange={(e) =>
-                  setNovoLote({ ...novoLote, desvioPadrao: e.target.value })
-                }
+                onChange={handleInputChange}
                 required
                 placeholder="Insira o desvio padrão"
               />
             </div>
             <div className="text-center">
-              <Link to="/" className="btn btn-secondary ms-1">
+              <Link to="/" className="btn btn-secondary btn-sm ms-1">
                 Sessão Anterior
               </Link>
               <button
                 type="button"
-                className="btn btn-primary ms-1"
-                onClick={handlePreviousLote}
+                className="btn btn-danger btn-sm ms-1"
+                onClick={handleDeleteLote}
               >
-                Período Anterior
+                Excluir
               </button>
               <button
                 type="button"
-                className="btn btn-primary ms-1"
+                className="btn btn-primary btn-sm ms-1"
+                onClick={handlePreviousLote}
+              >
+                Anterior
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary btn-sm ms-1"
                 onClick={handleNextLote}
               >
-                Próximo Período
+                {currentIndex === lotes.length ? "Salvar" : "Próximo"}
               </button>
-              <Link to="/cadastro-lotes" className="btn btn-secondary ms-1">
+              <Link
+                to="/cadastro-lotes"
+                className="btn btn-secondary btn-sm ms-1"
+              >
                 Próxima Sessão
               </Link>
             </div>
           </form>
+          <div className="mt-3">
+            <h3>Lotes Cadastrados</h3>
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">Período</th>
+                  <th scope="col">Análise</th>
+                  <th scope="col">Nº Lote</th>
+                  <th scope="col">Média</th>
+                  <th scope="col">DP</th>
+                </tr>
+              </thead>
+              <tbody>
+                {lotes.map((lote, index) => (
+                  <tr key={index}>
+                    <td>February 2023</td>
+                    <td>{lote.analise}</td>
+                    <td>{lote.numeroLote}</td>
+                    <td>{lote.media}</td>
+                    <td>{lote.desvioPadrao}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
